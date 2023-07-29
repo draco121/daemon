4fedf2f47dbb7865cde4bce5fd8b2f1934455a9e
Rajeev Chourey <rajeevchourey786@gmail.com>
2023-07-29 19:23:39 +0530

    added placeholders for constants

--- utils/constants.go
+++ utils/constants.go
@@ -2,11 +2,11 @@ package utils
 
 import "path"
 
-var CronString = "*/5 * * * *"
+var CronString = "{{cronstring}}"
 var RootDir = GetRootDir()
 
-const AppId = "64c3d38161c84e8b8005bdd9"
-const BaseUrl = "http://localhost:4200"
+const AppId = "{{appid}}"
+const BaseUrl = "{{baseurl}}"
 const UserName = "super.user"
 const Password = "Pa$$w0rd123!"
 
